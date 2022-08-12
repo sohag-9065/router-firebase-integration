9fc7868ba284a7e68dbfc1a712f6ce46487d617f
Sohag <sohaghossain7th@gmail.com>
2022-08-12 17:24:13 +0600

    replace custom hooks to firebase hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
+import { getAuth , signOut} from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
+// import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 
+const auth = getAuth(app);
+
 const Header = () => {
-    const {user, handleSignOut} = useFirebase();
+    // replace with firebase hooks 
+    // const {user, handleSignOut} = useFirebase();
+    const [user] = useAuthState(auth);
     return (
         <div className='header'>
             <nav>
@@ -15,7 +22,8 @@ const Header = () => {
                 <span style={{margin: '10px'}}>{user?.displayName && user.displayName}</span>
                 {
                     user?.uid ? 
-                    <button onClick={handleSignOut}>Sign Out</button>
+                    // <button onClick={handleSignOut}>Sign Out</button>
+                    <button onClick={() => signOut(auth)}>Sign Out</button>
                     :
                     <Link to="/login">Login</Link>
                 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,20 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    // use Firebase hooks from github . replace manual
-    const {signInWithGoogle} = useFirebase();
+    // use Firebase hooks from github . replace manual to hooks
+    // const {signInWithGoogle} = useFirebase();
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
     return (
         <div>
             <h3>Please Login</h3>
             <div style={{margin:'20px'}}>
-                <button onClick={signInWithGoogle}>Google Sign In</button>
+                {/* <button onClick={signInWithGoogle}>Google Sign In</button> */}
+                <button onClick={() => signInWithGoogle()}>Google Sign In</button>
             </div>
              <form >
                 
